471f275f2cbbd27e2be705043926968e21253908
olamijinadebayo <consultolamijin@gmail.com>
2018-04-12 15:39:45 +0300

    This will add a view function for user profiles

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired,ValidationError,Email,EqualTo
-
+from app.models import User
 class LoginForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired()])
     password = PasswordField('Password', validators = [DataRequired()])
@@ -23,6 +23,6 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different username')
 
     def validate_email(self,email):
-        user = User.query.filter_by(email = email.data)first()
+        user = User.query.filter_by(email = email.data).first()
         if user is not None:
             raise ValidationError('Pleease use a diifferent email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template,flash,redirect,url_for
-from app import app
-from app.forms import LoginForm
+from app import app,db
+from app.forms import LoginForm,RegistrationForm
 from flask_login import current_user,login_user,logout_user,login_required
 from app.models import User
 from flask import request
@@ -47,16 +47,25 @@ def logout():
     return redirect(url_for('index'))
 
 @app.route('/register', methods = ['GET','POST'])
-    def register():
-        if cuurent_user.is_authenticated:
-            return redirect(url_for('index'))
-        form = RegistrationForm()
-        if form.validate_on_submit():
-            user = User(username=form.username.data,email= form.email.data)
-            user.set_password(form.password.data)
-            db.session.add(user)
-            db.session.commit()
-            flash('Congratulations,you are now a registered user!')
-            return redirect(url_for('login'))
-        return render_template('register.html',title='Register', form = form)
-        
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,email= form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations,you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html',title='Register', form = form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user,'body':'Test post #1'}
+        {'author':user,'body':'Test post #2'}
+    ]
+    return render_template('user.html',user=user,posts=posts)
