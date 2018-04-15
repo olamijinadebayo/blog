5ea9d2b2dee95d5f287db069ecf023105817116d
olamijinadebayo <consultolamijin@gmail.com>
2018-04-15 18:56:36 +0300

    this will update the user template to display number users being followed and the users following the user

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
-from flask import render_template,flash,redirect,url_for
-from app import app,db
+from flask import render_template,flash,redirect,url_for,request
+from app import app, db
 from app.forms import LoginForm,RegistrationForm,EditProfileForm
 from flask_login import current_user,login_user,logout_user,login_required
-from app.models import User
-from flask import request
+from app.models import User,followers
 from werkzeug.urls import url_parse
 from datetime import datetime
 
