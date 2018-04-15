7056496044ad3f5ab81e8477b335418daafbfbea
olamijinadebayo <consultolamijin@gmail.com>
2018-04-15 16:57:22 +0300

    this will add methods to remove and append followers

--- app/models.py
+++ app/models.py
@@ -1,16 +1,18 @@
 from datetime import datetime
-from  app import db
+from  app import db,login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
 import urllib, hashlib
 from hashlib import md5
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
-class User(UserMixin,db.Model):
+followers = db.Table(
+    'followers',
+    db.Column('follower_id',db.Integer,db.ForeignKey('user.id')),
+    db.Column('followed_id',db.Integer,db.ForeignKey('user.id'))
+)
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index =True, unique=True)
     email = db.Column(db.String(120), index=True,unique = True)
@@ -19,10 +21,11 @@ class User(UserMixin,db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default = datetime.utcnow)
     followed = db.relationship(
-        'User', secondary= followers,
+        'User', secondary=followers,
         primaryjoin = (followers.c.follower_id ==id),
         secondaryjoin =(followers.c.followed_id == id),
         backref = db.backref('followers', lazy='dynamic'),lazy = 'dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -37,6 +40,22 @@ class User(UserMixin,db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def follow(self,user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self,user):
+        return self.followed.filter(
+        followers.c.followed_id == user.id).count()>0
+
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -47,8 +66,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-followers = db.Table('followers',
-    db.Column('follower_id',db.Integer,db.ForeignKey('user_id')),
-    db.Column('followed_id',db.Integer,db.ForeignKey('user.id'))
-)
