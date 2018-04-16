e2387898b29bf99867044549223f2b956e3e56fe
olamijinadebayo <consultolamijin@gmail.com>
2018-04-16 06:59:18 +0300

    this will also implement pagination in the user route and template while setting the post per page in the config file to 25

--- app/routes.py
+++ app/routes.py
@@ -29,8 +29,9 @@ def index():
     next_url = url_for('index', page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
-    return render_template('index.html', title ='Home Page',posts = posts.items,
-    form = form,next_url=next_url,prev_url=prev_url)
+         if posts.has_prev else None
+    return render_template('index.html', title ='Home',posts=posts.items,
+        form = form,next_url=next_url,prev_url=prev_url)
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
@@ -73,12 +74,15 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author':user,'body':'Test post #1'},
-        {'author':user,'body':'Test post #2'}
-    ]
-    return render_template('user.html',user=user,posts=posts)
-
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 @app.route('/edit_profile', methods=['GET','POST'])
 @login_required
 def edit_profile():
