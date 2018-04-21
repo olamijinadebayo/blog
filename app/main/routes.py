e457cfabcf2f5b558c4424a14f9c96f6d1d9be6e
olamijinadebayo <consultolamijin@gmail.com>
2018-04-21 16:13:36 +0300

    this will fix some errors in the naming of routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -21,7 +21,9 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
+
         post = Post(body=form.post.data, author=current_user)
+        db.session.add(title)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
