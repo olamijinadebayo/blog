0bb933923e25218a0638a6224bf49ec4bc0b90c8
olamijinadebayo <consultolamijin@gmail.com>
2018-04-21 11:04:45 +0300

    this will change the overall folder structure to make sure that the app is scalable by categorizing the app using three major blueprints in the application

--- tests.py
+++ tests.py
@@ -2,7 +2,13 @@ from datetime import datetime, timedelta
 import unittest
 from app import app, db
 from app.models import User, Post
+from config import Config
 
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+    
 class UserModelCase(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
