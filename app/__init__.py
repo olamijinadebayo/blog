e945756eb0c33af580ef4bf9f1c0cc6ce911e32d
olamijinadebayo <consultolamijin@gmail.com>
2018-04-23 18:30:08 +0300

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
